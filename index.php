b90f62eed24397acdaebe9553eb699b0074e0e94
pwfgitdone <ddrdan7@hotmail.com>
2021-12-17 03:51:32 -0300

    bgcolor alterado para white em index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-$titulo = "Exemplo";
+ $titulo = "Exemplo";
 ?>
 <html>
 <head>
     <title>Exemplo</title>
 </head>
-<body style="background-color: gray;">
+<body style="background-color: white;">
     <h1><?php print $titulo; ?></h1>
     <p>Exemplo de CI/CD utilizando VSCODE, GITHUB, AWS CODEPIPELINE e AWS ELASTIC BEANSTALK</p>
 </body>
